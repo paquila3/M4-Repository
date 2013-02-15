255f3b9f612a300e7293c3909349a8a4d697046b
Peter <Peter@128.61.126.156>
2013-02-15 14:30:07 -0500

    Me doing the Person1 file. Nobody else do Person1.

--- M4/src/edu/gatech/oad/antlab/person/Person1.java
+++ M4/src/edu/gatech/oad/antlab/person/Person1.java
@@ -30,8 +30,10 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	  String res = input.charAt(0) + input.charAt(1);
+	  for (int i = 2; i < input.length(); i++)
+		  res = input.charAt(i) + res;
+	  return res;
 	}
 	
 	/**
